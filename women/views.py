c3ef341175081f54dc51c5203da517d01364cccb
user <dmitriy.buzoveria@cruxlab.com>
2022-06-18 16:48:10 +0300

    app some templatetags

--- women/views.py
+++ women/views.py
@@ -3,12 +3,6 @@ from django.http import HttpResponse, Http404
 
 from .models import *
 
-menu = [{'title': 'О сайте', 'url_name': 'about'},
-    {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'}
-]
-
 
 def pageNotFound(request, exception):
     return HttpResponse('<h1>Sorry, but something went wrong, see you later!</h1>')
@@ -16,20 +10,16 @@ def pageNotFound(request, exception):
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts': posts,
-        'cats': cats,
-        'menu': menu,
         'title': 'The main page',
         'cat_selected': 0
     }
-    return render(request, 'women/index.html',context=context)
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     context = {
-        'menu': menu,
         'title': 'About the site'
     }
     return render(request, 'women/about.html', context=context)
@@ -53,15 +43,12 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'cats': cats,
-        'menu': menu,
         'title': 'Отображение по рубрикам',
         'cat_selected': cat_id
     }
